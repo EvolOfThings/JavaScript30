8fb0453bff2002232022f3c5a2dd39311b88a4bb
RishadYamnoor <rishad.yamnoor@gmail.com>
2019-01-14 20:31:45 +0530

    sort method to get age by oldest to younger

--- projects/04 - Array Cardio Day 1/main.js
+++ projects/04 - Array Cardio Day 1/main.js
@@ -24,5 +24,12 @@
     //     return total + (inventor.passed - inventor.year);
     // }, 0);
 
+   // 5. Sort the inventors by years lived
+// const oldest = inventors.sort((a, b) => {
+//     const lastInventor = a.passed - a.year ;
+//     const nextInventor = b.passed - b.year ;
+//     return lastInventor > nextInventor ? -1 : 1;
+// })
+
 
 
